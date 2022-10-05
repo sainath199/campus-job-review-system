3950cf547bb758869da05a9b5a9a163c5e0cea24
Prachit Mhalgi <prachitmhalgi@gmail.com>
2022-10-04 21:14:53 -0400

    Initial templates for flask app

---
+++ app/models.py
@@ -0,0 +1,8 @@
+from app import db
+
+class Reviews(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    job_title = db.Column(db.String(64), index=True, nullable=False)
+    job_description = db.Column(db.String(120), index=True, nullable=False)
+    review = db.Column(db.String(120), index=True, nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
---
+++ app/routes.py
@@ -0,0 +1,60 @@
+from flask import render_template, request, redirect
+from app import app, db
+from app.models import Reviews
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    entries = Reviews.query.all()
+    return render_template('index.html', entries=entries)
+
+
+@app.route('/add', methods=['POST'])
+def add():
+    if request.method == 'POST':
+        form = request.form
+        title = form.get('job_title')
+        description = form.get('job_description')
+        review = form.get('review')
+        rating = form.get('rating')
+        entry = Reviews(job_title = title, job_description = description, review=review, rating=rating)
+        db.session.add(entry)
+        db.session.commit()
+        return redirect('/')
+
+
+@app.route('/update/<int:id>')
+def updateRoute(id):
+    if not id or id != 0:
+        entry = Reviews.query.get(id)
+        if entry:
+            return render_template('update.html', entry=entry)
+
+
+@app.route('/update/<int:id>', methods=['POST'])
+def update(id):
+    if not id or id != 0:
+        entry = Reviews.query.get(id)
+        if entry:
+            form = request.form
+            title = form.get('job_title')
+            description = form.get('job_description')
+            review = form.get('review')
+            rating = form.get('rating')
+            entry.title = title
+            entry.description = description
+            entry.review = review
+            entry.rating = rating
+            db.session.commit()
+        return redirect('/')
+
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    if not id or id != 0:
+        entry = Reviews.query.get(id)
+        if entry:
+            db.session.delete(entry)
+            db.session.commit()
+        return redirect('/')
