8e0c3094f8781bacdc7f85e47d9ecc0c9f5a6f33
= <=>
2022-10-08 16:53:11 -0400

    fixes #9 add filter

--- app/routes.py
+++ app/routes.py
@@ -11,9 +11,17 @@ def review():
 
 @app.route('/pageContent')
 def page_content():
-
         entries = Reviews.query.all()
         return render_template('page_content.html', entries=entries)
+
+@app.route('/pageContentPost', methods=['POST'])
+def page_content_post():
+        print("hi")
+        if request.method == 'POST':
+            form = request.form
+            search_title = form.get('search')
+            entries = Reviews.query.filter_by(job_title=search_title)
+            return render_template('page_content.html', entries=entries)
 
 @app.route('/')
 @app.route('/home')
