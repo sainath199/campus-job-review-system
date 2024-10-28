cf94e33c602c9f428ec19063d4ceaf39f971e40e
Hasitha257 <yhasitha1@gmail.com>
2024-10-27 20:57:06 -0400

    added profile and resume forms

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import (
     StringField,
     PasswordField,
@@ -11,8 +12,18 @@ from wtforms import (
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from app.models import User
 
+class UpdateAccountForm(FlaskForm):
+    """Form for updating account details."""
+    username = StringField('Name', validators=[DataRequired(), Length(min=2, max=50)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    contact_info = StringField("Contact Info", validators=[Length(max=255)])  # Add this line
+    submit = SubmitField('Update Account')
+    address=StringField("Address",validators=[Length(max=255)])
+
+
 
 class RegistrationForm(FlaskForm):
+    """Form for new user registration."""
     username = StringField(
         "Username", validators=[DataRequired(), Length(min=2, max=20)]
     )
@@ -37,6 +48,7 @@ class RegistrationForm(FlaskForm):
 
 
 class LoginForm(FlaskForm):
+    """Form for user login."""
     email = StringField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
     remember = BooleanField("Remember Me")
@@ -44,8 +56,9 @@ class LoginForm(FlaskForm):
 
 
 class ReviewForm(FlaskForm):
+    """Form for submitting job reviews."""
     department = StringField("Department", validators=[DataRequired()])
-    locations = StringField("Location", validators=[DataRequired()])
+    location = StringField("Location", validators=[DataRequired()])  # Fixed typo from 'locations' to 'location'
     job_title = StringField("Job Title", validators=[DataRequired()])
     job_description = StringField("Job Description", validators=[DataRequired()])
     hourly_pay = StringField("Hourly Pay", validators=[DataRequired()])
@@ -54,22 +67,25 @@ class ReviewForm(FlaskForm):
     rating = RadioField(
         "Rating",
         validators=[DataRequired()],
-        choices=[(1, "1"), (2, "2"), (3, "3"), (4, "4"), (5, "5")],
+        choices=[('1', "1"), ('2', "2"), ('3', "3"), ('4', "4"), ('5', "5")],
     )
     recommendation = RadioField(
         "Recommendation",
         validators=[DataRequired()],
-        choices=[
-            (1, "1"),
-            (2, "2"),
-            (3, "3"),
-            (4, "4"),
-            (5, "5"),
-            (6, "6"),
-            (7, "7"),
-            (8, "8"),
-            (9, "9"),
-            (10, "10"),
-        ],
+        choices=[(str(i), str(i)) for i in range(1, 11)],  # Fixed the line that got cut off
     )
     submit = SubmitField("Submit your review")
+
+
+class UserProfileForm(FlaskForm):
+    """Form for updating user profile information."""
+    name = StringField("Name", validators=[DataRequired(), Length(max=100)])
+    contact_info = StringField("Contact Info", validators=[Length(max=255)])
+    address = StringField("Address", validators=[Length(max=255)])
+    submit = SubmitField("Update Profile")
+
+
+class ResumeUploadForm(FlaskForm):
+    """Form for uploading resumes."""
+    resume= FileField("Upload Resume", validators=[DataRequired(), FileAllowed(['pdf', 'doc', 'docx'], 'Documents only!')])
+    submit = SubmitField("Upload Resume")
